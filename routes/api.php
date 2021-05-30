da7d9b5fd31298dd29b00b93e1e724b9f1837905
Dana Sugu <dana.sugu@gmail.com>
2021-05-30 11:05:48 +0300

    faker - authors

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,8 @@ Route::middleware('auth:api')->prefix('v1')->group(function() {
     Route::get('/user', function(Request $request){
         return $request->user();
     });
+
+    Route::get('/authors/{author}', [AuthorsController::class, 'show']);
 });
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+
