a3773f4222ccc00c74403e46eabb4a2c2ae4717a
Dana Sugu <dana.sugu@gmail.com>
2021-06-06 08:07:38 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\AuthorsController;
 use App\Models\Author;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthorsController;
+use App\Http\Controllers\BooksController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -30,7 +30,7 @@ Route::middleware('auth:api')->prefix('v1')->group(function() {
     // Route::get('/authors', [AuthorsController::class, 'index']);
 
     Route::apiResource('/authors', AuthorsController::class, );
-    
+    Route::apiResource('/books', BooksController::class, );    
 });
 
 
